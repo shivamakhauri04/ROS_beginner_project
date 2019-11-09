0d1eed87c79d94b9280ca8f3efa939cb3fecfebd
shivamakhauri04 <shivamakhauri04@gmail.com>
2019-11-09 11:54:39 -0500

    modified the talker to add the tf broadcaster. run the tfview and generated the frames pdf and added to the repository. Updated the service.launch

--- src/talker.cpp
+++ src/talker.cpp
@@ -21,86 +21,50 @@ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
 /**
 * @file talker.cpp
 * @author Shivam Akhauri 
-* @date 31 October 2019
+* @date 7 November 2019
 * @copyright 2019 Shivam Akhauri
 * @brief Contains the service to check for the details of a subject 
 * in the subject database of University of Maryland
 */
 
 #include <ros/ros.h>
-#include <beginner_tutorials/exampleServiceMessage.h>
-#include <iostream>
-#include <string>
+#include <tf/transform_broadcaster.h>
+#include <turtlesim/Pose.h>
 
+std::string turtle_name;
 
 /**
-* @brief function to provide the service to check 
-* for a subject from the subject database. It returns the subject name,
-* Number of credits associated with the subject, if the suject is robotics
-* subject or CMSC subject.
-* The client sends the request to this service in form of a subject code
-* The service analyses the request to check if it is in the database
-* @params beginner_tutorials :: exampleServiceMessageRequest& request 
-* <generated in the exampleServiceMessage.h in devel/include 
-* upon adding .srv to the package 
-* @params beginner_tutorials :: exampleServiceMessageResponse& response
-* <generated in the exampleServiceMessage.h in devel/include 
-* upon adding .srv to the package 
-* @return bool: if the service responses
+* @brief function poseCallback
+* @param const turtlesim::PoseConstPtr&
+* @return none
+* calculates the robot frame transformations 
+* between the robot frames and world frame 
 */
-bool callback(beginner_tutorials :: exampleServiceMessageRequest &request,
-beginner_tutorials :: exampleServiceMessageResponse &response) {
-  ROS_INFO("callback activated");
-  // convert the client data to a C++-class string, to use member funcs
-  std::string in_name(request.subject);
-  response.onList = false;
-  // create a small database.
-  if (in_name.compare("ENPM808X") == 0) {
-    ROS_INFO_STREAM("asked about 808X");
-    response.credits = 3;
-    response.roboticsCourse = true;
-    response.onList = true;
-    response.subjectName = "Robotics SOftware Development";
-  }
 
-  if (in_name.compare("ENPM662") == 0) {
-    ROS_INFO_STREAM("asked about Robot Modelling");
-    response.credits = 3;
-    response.roboticsCourse = true;
-    response.onList = true;
-    response.subjectName = "Robot Modelling";
-  }
-
-  if (in_name.compare("CMSC440") == 0) {
-    ROS_INFO_STREAM("asked about DataStructures");
-    response.credits = 2;
-    response.roboticsCourse = false;
-    response.onList = true;
-    response.subjectName = "Datastructures and Algorithms";
-  }
-  return true;
+void poseCallback(const turtlesim::PoseConstPtr& msg){
+  // create transform broadcaster to publish the transformations
+  static tf::TransformBroadcaster br;
+  tf::Transform transform;
+  // set the origin
+  transform.setOrigin( tf::Vector3(msg->x, msg->y, 0.0) );
+  tf::Quaternion q;
+  q.setRPY(0, 0, msg->theta);
+  // set the rotations
+  transform.setRotation(q);
+  // send the calculated transform
+  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", turtle_name));
 }
 
-/**
-* @brief main function. creates a service node
-* creates a service and advertises it over ROS
-*/
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "beginner_tutorials_server");
-  ros::NodeHandle n;
-  if (!ros::isInitialized()) {
-    ROS_FATAL_STREAM("The Ros node for service not initialized");
-  }
-  ROS_DEBUG_STREAM("Ready to check database for the subjects");
-  ros::ServiceServer service = n.advertiseService
-  ("lookup_by_subject_code", callback);
-  int rate = 10;
-  if (argv[1] == "slow") {
-    rate = 20;
-  }
-  ros::Rate loop_rate(rate);
-  loop_rate.sleep();
+int main(int argc, char** argv){
+  // create the node
+  ros::init(argc, argv, "Talk_with_parent_world");
+  if (argc != 2){ROS_ERROR("need turtle name as argument"); return -1;};
+  turtle_name = argv[1];
+  // create the node handle
+  ros::NodeHandle node;
+  // call the broadcaster to calculate transform between the frames
+  ros::Subscriber sub = node.subscribe(turtle_name+"/pose", 10, &poseCallback);
   ros::spin();
-
   return 0;
-}
+};
+
